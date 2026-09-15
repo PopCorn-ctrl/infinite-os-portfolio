8895139e0058a999e0df452946f6916050370997
PopCorn-ctrl <evan100407@gmail.com>
2026-09-15 21:07:58 +0200

    Refactor code structure for improved readability and maintainability

--- www/js/cursor.js
+++ www/js/cursor.js
@@ -10,3 +10,16 @@ document.addEventListener('click', e => {
         cursor.classList.remove("expand");
     }, 500);
 });
+
+
+const customMenu = document.getElementById("customMenu");
+document.addEventListener("contextmenu", (event) => {
+  event.preventDefault();
+  //Set positiom of the custom menu to where user clicked
+  customMenu.style.top = `${event.pageY}px`;
+  customMenu.style.left = `${event.pageX}px`;
+  customMenu.style.display = "block";
+});
+document.addEventListener("click", () => {
+  customMenu.style.display = "none";
+});
--- www/js/preload.js
+++ www/js/preload.js
@@ -1,40 +1,28 @@
-
-const loginButton = document.querySelector('.loginbutton');
 const TEST = document.querySelector('.cursor');
 const startup = document.getElementById('startup');
 
 window.addEventListener('load', () => {
+
+    const loadingScreen = document.querySelector('.loading');
+
     setTimeout(() => {
-        const loadingScreen = document.querySelector('.loading');
-        loadingScreen.style.opacity = '0';
-        loadingScreen.style.pointerEvents = 'none';
 
-        setTimeout(() => loadingScreen.remove(), 600);
-    }, 5000);   
-    setTimeout(() => {
-        document.querySelector(".clock").classList.add("show");
-    }, 5300);
-    setTimeout(() => {
-        document.querySelector(".loginbutton").classList.add("show");
-    }, 5600);
+        // Lance le swipe diagonal
+        loadingScreen.classList.add('hide');
+
+        // Révèle la page
+        startup.style.opacity = '1';
+        startup.style.transform = 'scale(1)';
+        startup.style.filter = 'blur(0)';
+
+        // Supprime le preload après l'animation
+        setTimeout(() => {
+            loadingScreen.remove();
+        }, 4000);
+
+    }, 5000);
+
 });
-
-loginButton.addEventListener('mouseenter', () => {
-    TEST.classList.add('expand');
-});
-
-loginButton.addEventListener('mouseleave', () => {
-    TEST.classList.remove('expand');
-});
-
-loginButton.addEventListener('click', () => {
-    startup.style.transform = 'translateY(-100%)';
-    startup.style.transition = 'transform 1s ease-in-out';
-    wait(500).then(() => {
-        startup.style.display = 'none';
-    });
-});
-
 
 particlesJS.load('particles-js', 'www/js/particlesjs-config.json', function () {
     console.log('Particles.js chargé');
